99b1966e7499e5ee0a7f99e8deace0c8fcfc916e
leedonyoeb <duq2509@naver.com>
2020-10-19 20:22:28 +0900

    hi

---
+++ app/src/main/java/com/google/texxxi/MainActivity.java
@@ -0,0 +1,37 @@
+package com.google.texxxi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.ViewGroup;
+
+
+import net.daum.android.map.MapView;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MapView mapView = new MapView(this);
+        ViewGroup mapViewContainer = findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+
+
+
+    }
+
+
+}
