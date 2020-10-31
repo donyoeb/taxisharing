330bdcf57cb6c76bea575fec8f41e3054db4082c
leedonyoeb <duq2509@naver.com>
2020-10-31 14:54:16 +0900

    로딩화면 수정, 로그인화면 생성

--- app/src/main/java/com/google/texxxi/MenuActivity.java
+++ app/src/main/java/com/google/texxxi/MenuActivity.java
@@ -1,6 +1,15 @@
 package com.google.texxxi;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class MenuActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+
+    }
 }
+
+
--- app/src/main/java/com/google/texxxi/loginActivity.java
+++ app/src/main/java/com/google/texxxi/loginActivity.java
@@ -1,6 +1,26 @@
 package com.google.texxxi;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import net.daum.mf.map.api.MapView;
+
 
 public class loginActivity extends Activity {
+    private DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mReference;
+    private ChildEventListener mChild;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+
 }
