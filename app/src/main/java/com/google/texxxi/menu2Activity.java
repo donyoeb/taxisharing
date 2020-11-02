8786100c01488e421aa1d782b7eb39c0a75e402f
leedonyoeb <duq2509@naver.com>
2020-11-02 16:32:02 +0900

    메뉴 이벤트 기능(확인버튼설정)

--- app/src/main/java/com/google/texxxi/menu2Activity.java
+++ app/src/main/java/com/google/texxxi/menu2Activity.java
@@ -1,7 +1,9 @@
 package com.google.texxxi;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
@@ -12,7 +14,8 @@ public class menu2Activity extends Activity implements TimePicker.OnTimeChangedL
     private TextView ttv;  //타임 텍스트뷰
     private TimePicker tp; //타임 피커
     Calendar c; //캘린더
-
+    private String name;
+    private int mflag;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,13 +28,41 @@ public class menu2Activity extends Activity implements TimePicker.OnTimeChangedL
         int hourofday = c.get(c.HOUR_OF_DAY);
         int minute = c.get(c.MINUTE);
 
-        ttv.setText("현재 시간\n" + hourofday + ":" + minute);
+        ttv.setText("현재 시간\n  " + hourofday + ":" + minute);
 
         tp.setOnTimeChangedListener(this); // 시간, 분 선택
+
+        Intent intent = getIntent();
+        name =intent.getExtras().getString("닉네임");
+        mflag =intent.getExtras().getInt("방버튼");
+
 
     }
 
     public void onTimeChanged(TimePicker view, int hourofday, int minute) {
-        ttv.setText("설정된 시간\n" + hourofday + ":" + minute);  // 선택한 시간, 분  설정
+        ttv.setText("설정된 출발 시간\n         " + hourofday + ":" + minute);  // 선택한 시간, 분  설정
     }
+
+    public void mOnClose(View v){ //확인버튼 클릭 이벤트
+
+        if (mflag==0){ // 방 생성= 0
+
+        }
+        else if(mflag==1){ //방 검색 =1
+
+        }
+        else{
+
+        }
+
+        //데이터 전달하기
+        Intent intent = new Intent();
+        intent.putExtra("result", "Close Popup");
+        setResult(RESULT_OK, intent);
+
+        //액티비티(팝업) 닫기
+        finish();
+    }
+
+
 }
