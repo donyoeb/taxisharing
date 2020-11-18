568857d9309f51ec9b0f138943916b0fdb1f0b58
leedonyoeb <duq2509@naver.com>
2020-11-18 23:02:37 +0900

    방 검색할때 조건설정해줌

--- app/src/main/java/com/google/texxxi/MenuActivity.java
+++ app/src/main/java/com/google/texxxi/MenuActivity.java
@@ -4,10 +4,13 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -17,6 +20,8 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import androidx.annotation.NonNull;
 
 public class MenuActivity extends Activity {
 
@@ -52,8 +57,6 @@ public class MenuActivity extends Activity {
     ArrayList<String> starth = new ArrayList<String>();  //출발 시 리스트
     ArrayList<String> startm = new ArrayList<String>();  // 출발 분 리스트
 
-    private String result;
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
@@ -71,6 +74,43 @@ public class MenuActivity extends Activity {
         menutv = (TextView)findViewById(R.id.menutv); //메뉴 텍스트뷰
 
         listv =(ListView)findViewById(R.id.roomlist); //방 리스트뷰
+
+
+        mReference = mDatabase.getReference().child("방");
+        mReference.addValueEventListener(new ValueEventListener() { //처음 들어오면 데이터베이스에 저장되어있는 유저 리스트 가져와서 나중에 비교할때 사용하게 하기
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                usern.clear(); //올때마다 클리어 ( 나중에 버튼하나 누르면 클리어하고 다시 받아오는 기능 추가)
+                starts.clear();
+                arraives.clear();
+                starth.clear();
+                startm.clear();
+
+                for (DataSnapshot i : dataSnapshot.getChildren()) {
+
+                    String u2 = i.getKey(); //유저정보 가져오기
+                    String ss2 = i.child("1출발지").getValue().toString();
+                    String as2 = i.child("2도착지").getValue().toString();
+                    String sh2 = i.child("3출발시").getValue().toString();
+                    String sm2 = i.child("4출발분").getValue().toString();
+
+                    usern.add(u2);   //하나하나 다 리스트에 저장시켜둠 나중에 쓰기위해
+                    starts.add(ss2);
+                    arraives.add(as2);
+                    starth.add(sh2);
+                    startm.add(sm2);
+
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+
+
+
+
 
         mnewbt.setOnClickListener(new View.OnClickListener() {   //방 생성 버튼
             @Override
@@ -100,8 +140,6 @@ public class MenuActivity extends Activity {
         });
 
 
-
-
     }
 
     @Override
@@ -115,67 +153,44 @@ public class MenuActivity extends Activity {
                 adapter = new room_list_adapter();
                 listv.setAdapter(adapter); //리스트뷰에 넣을 어댑터 설정
 
-                returnhour = data.getStringExtra("리턴시");
-                returnmin = data.getStringExtra("리턴분");
                 returnstart = data.getStringExtra("출발지리턴");
                 returnarrive = data.getStringExtra("도착지리턴");
+                returnhour = data.getStringExtra("리턴시");
+                returnmin = data.getStringExtra("리턴분");
+
+                int rh = Integer.parseInt(returnhour); //조건 적용을 위해 숫자로 변환
+                int rm = Integer.parseInt(returnmin);
 
                 // 데이터베이스에서 위에 가져온 정보를 토대로 검색한 결과를 리스트에 담아서 리스트뷰에 보여주기 ㄱㄱㄱㄱㄱㄱㄱ
-                menutv.setText("방 찾기 기능 설정값  ( 출발지 / 도착지 / 출발시 : 출발분) \n  ->   "+returnstart+" / "+returnarrive+" / "+returnhour+" : "+returnmin);
-
-                mReference = mDatabase.getReference("방"); // 변경값을 확인할 child 이름
-                mReference.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
+                menutv.setText("방 찾기 기능 설정값  ( 출발지 / 도착지 / 출발시 : 출발분) \n  ->   " + returnstart + " / " + returnarrive + " / " + returnhour + " : " + returnmin);
 
 
-                        for (DataSnapshot i : dataSnapshot.getChildren()) {
+                int l_size = usern.size();
 
-                            String username = i.getKey(); //방-공통방의 키값 (유저 닉넴)
+                for (int i = 0; i < l_size; i++) {
 
-                            String startspot = i.child("1출발지").getValue().toString();   //키값(유저 닉네임에 저장된 값들 가져오기
-                            String arrivespot = i.child("2도착지").getValue().toString();
-                            String starthour = i.child("3출발시").getValue().toString();
-                            String startmin = i.child("4출발분").getValue().toString();
+                    int rh_2 = Integer.parseInt(starth.get(i)); // 데이터베이스에 저장된 i번째 시간
+                    int rm_2 = Integer.parseInt(startm.get(i));  // i번째 분
 
+                    if (returnstart.equals(starts.get(i)) && returnarrive.equals(arraives.get(i))) {
 
-
-                            usern.add(username);   //하나하나 다 리스트에 저장시켜둠 나중에 쓰기위해
-                            starts.add(startspot);
-                            arraives.add(arrivespot);
-                            starth.add(starthour);
-                            startm.add(startmin);
-
-                            adapter.addItem(startspot,arrivespot,starthour,startmin); //adapter에 저장
-
-
-                        }
-
-                        int l_size = usern.size();
-
-                        for(int i=0; i<l_size; i++){
-
-                            if(returnstart == starts.get(i)){
-                                if(returnarrive == arraives.get(i)){
-
-                                }
+                        if(rh_2 == rh) {   //시간이 같을때만 분까지 비교
+                            if (rm_2 >= rm){
+                                adapter.addItem(starts.get(i), arraives.get(i), starth.get(i), startm.get(i)); //adapter에 저장
                             }
-
-//리스트뷰 오류 해결, 커스텀리스트뷰로 수정
+                        }
+                        else if(rh_2 > rh){ // 시간이 더 크면 그냥 바로 확인
+                            adapter.addItem(starts.get(i), arraives.get(i), starth.get(i), startm.get(i)); //adapter에 저장
 
                         }
+                        else{
 
-                        adapter.notifyDataSetChanged(); // 어댑터리스트 갱신
-                        listv.setSelection(adapter.getCount() - 1);
-
-
+                        }
                     }
 
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-
-                    }
-                });
+                }
+                adapter.notifyDataSetChanged(); // 어댑터리스트 갱신
+                listv.setSelection(adapter.getCount() - 1);
 
 
             } else if (resultCode == RESULT_CANCELED) {
@@ -183,8 +198,22 @@ public class MenuActivity extends Activity {
             }
         }
 
+//////////////리스트뷰 클릭이벤트부터 시작하면 됨
+        listv.setOnItemClickListener(new AdapterView.OnItemClickListener() {    //리스트뷰 클릭이벤트
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long idinlist) {
+
+                Toast.makeText(MenuActivity.this,"닉네임 : "+usern.get(position),Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
 
     }
+
+
     private void initDatabase() {
 
         mDatabase = FirebaseDatabase.getInstance();
